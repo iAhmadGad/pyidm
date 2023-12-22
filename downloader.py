ee4711cb6847f16b2af2b97249590268954305f3
iAhmadGad <ahmadasaadgad@gmail.com>
2023-12-23 00:21:24 +0300

    Delete downloader.py in wrong directory

--- downloader.py
+++
@@ -1,5 +0,0 @@
-from urllib.request import urlretrieve
-
-def download(url, pathname):
-   path, headers =  urlretrieve(url, pathname)
-   print(filename, "(", filesize.calculateSize(int(headers["Content-Length"])), ")", "succesfuly downloaded!")
