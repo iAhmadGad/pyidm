02f80e080a5fcb3f6c0a92b25760d23357df84a5
iAhmadGad <ahmadasaadgad@gmail.com>
2023-12-23 00:13:12 +0300

    Create configurator.py

---
+++ src/configurator.py
@@ -0,0 +1,17 @@
+import json
+
+def getConfigurations():
+    configfile = open(".pyidmconfig.json")
+    return json.loads(configfile)
+
+def getConfiguration(key):
+    return getConfigurations()[key]
+
+def configure(key, value):
+    
+    configurations = getConfigurations()
+    configurations[key] = value
+
+    configfile = open(".pyidmconfig.json", "w")
+    configfile.write(json.dumps(configurations))
+    configfile.close()
